3f867fca057ee6df5704d038f6ed0b1d100b304f
Tobias Bocanegra <tripod@adobe.com>
2023-01-27 15:13:26 +0100

    improve

--- mixtures/index.js
+++ mixtures/index.js
@@ -1,28 +1,28 @@
 /* global readline, print */
 
-function solve(colors, p, l, cache = new Map(), d = 0) {
+function solve(colors, p, l, cache) {
   // console.log(' '.repeat(d), p, p + l)
-  if (l === 1) {
-    return [colors[p], 0];
-  }
-  if (l === 2) {
-    return [(colors[p] + colors[p + 1]) % 100, colors[p] * colors[p + 1]];
-  }
-  const key = `${p},${l}`;
-  let best = cache.get(key);
+  let best = cache[p][l];
   if (!best) {
-    best = [0, Number.MAX_SAFE_INTEGER];
-    // generate all possible segmentations
-    for (let i = 1; i < l; i++) {
-      const [c0, s0] = solve(colors, p, i, cache, d + 1);
-      const [c1, s1] = solve(colors, p + i, l - i, cache, d + 1);
-      const c = (c0 + c1) % 100;
-      const s = (c0 * c1) + s0 + s1;
-      if (s < best[1]) {
-        best = [c, s];
+    if (l === 2) {
+      best = [(colors[p] + colors[p + 1]) % 100, colors[p] * colors[p + 1]];
+    } else {
+      // generate all possible segmentations
+      let bestColor = 0;
+      let bestSmoke = Number.MAX_SAFE_INTEGER;
+      for (let i = 1; i < l; i++) {
+        const [c0, s0] = solve(colors, p, i, cache);
+        const [c1, s1] = solve(colors, p + i, l - i, cache);
+        const c = (c0 + c1) % 100;
+        const s = (c0 * c1) + s0 + s1;
+        if (s < bestSmoke) {
+          bestSmoke = s;
+          bestColor = c;
+        }
       }
+      best = [bestColor, bestSmoke];
     }
-    cache.set(key, best);
+    cache[p][l] = best;
   }
   return best;
 }
@@ -33,6 +33,11 @@ let line;
 while (line = readline()) {
   const N = parseInt(line, 10);
   const colors = readline().split(/\s+/).map((d) => parseInt(d, 10));
-  results.push(solve(colors, 0, N)[1]);
+  const cache = new Array(N);
+  for (let i = 0; i < N; i++) {
+    cache[i] = new Array(N);
+    cache[i][1] = [colors[i], 0];
+  }
+  results.push(solve(colors, 0, N, cache)[1]);
 }
 print(results.join('\n'));
